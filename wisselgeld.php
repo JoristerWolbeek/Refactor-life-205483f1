18f7f5c3c5b21d0a7537f9e9cf32f6ac5843d5ff
Joris ter Wolbeek <21074092@talnet.nl>
2020-02-06 09:57:12 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,41 @@
+<?php
+$munten=array (
+    "50 euro"=>0,
+    "20 euro"=>0,
+    "10 euro"=>0,
+    "5 euro"=>0,
+    "2 euro"=>0,
+    "1 euro"=>0,
+    "0.50 cent"=>0,
+    "0.20 cent"=>0,
+    "0.10 cent"=>0,
+    "0.05 cent"=>0,
+);
+
+function getValue(){
+    $bedrag = doubleval($argv[1]);
+    $restBedrag = round($bedrag, 2, PHP_ROUND_HALF_UP);
+    makeAndGiveCoin();
+}
+
+function makeAndGiveCoin(){
+    foreach($munten as $munt => $hoeveelMunt) {
+        $muntFix = doubleval($munt);
+        while($restBedrag >= $muntFix) {
+            $munten[$munt]++;
+            $restBedrag = round($restBedrag-$muntFix, 2);
+        }
+    }
+    printCoin();
+}
+
+function printCoin(){
+    foreach($munten as $munt=>$hoeveelMunt) {
+        $muntFix = doubleval($munt);
+        if($hoeveelMunt >= 1) {
+            $tussen=explode(" ", $munt);
+            echo($muntFix." ".$tussen[1]." x ".$hoeveelMunt.PHP_EOL);
+        }
+    }
+}
+
